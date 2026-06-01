1ebbc49056599944ee0c9002f25a32a5d9a8da9a
Bartek Ogórkiewicz <bartosz.ogorkiewicz@gmail.com>
2026-06-01 07:51:24 +0200

    setup: vscode config + template

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+
+    return 0;
+}
